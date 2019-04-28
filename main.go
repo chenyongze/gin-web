9493a9c8b70788a9260b88f75c9fb4a2e93789bf
yongze.chen <sapphire.php@gmail.com>
2019-04-28 16:00:34 +0800

    add json response

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
 
-func main() {
-    r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
+// func1: 处理最基本的GET
+func func1 (c *gin.Context)  {
+    // 回复一个200OK,在client的http-get的resp的body中获取数据
+    c.String(http.StatusOK, "test1 OK")
+}
+// func2: 处理最基本的POST
+func func2 (c *gin.Context) {
+    // 回复一个200 OK, 在client的http-post的resp的body中获取数据
+    c.JSON(http.StatusOK, gin.H{
+        "status":0,
+        "data":gin.H{
+            "a":"b",
+        },
     })
-    r.Run(":8083") // listen and serve on 0.0.0.0:8080
+}
+func main(){
+    // 注册一个默认的路由器
+    router := gin.Default()
+    // 最基本的用法
+    router.GET("/test1", func1)
+    router.GET("/test2", func2)
+    // 绑定端口是8888
+    router.Run(":8086")
 }
