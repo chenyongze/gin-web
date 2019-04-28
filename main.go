2b4e28df97a808969f68ebfb5da3a16495b23898
yongze.chen <sapphire.php@gmail.com>
2019-04-28 16:09:10 +0800

    router group

--- main.go
+++ main.go
@@ -20,12 +20,23 @@ func func2 (c *gin.Context) {
         },
     })
 }
+
+func func3 (c *gin.Context)  {
+    //回复一个200OK,在client的http-get的resp的body中获取数据
+    //c.String(http.StatusOK, "v1 group")
+    c.JSON(http.StatusOK, gin.H{
+        "l":"group v1",
+    })
+}
 func main(){
     // 注册一个默认的路由器
     router := gin.Default()
     // 最基本的用法
     router.GET("/test1", func1)
     router.GET("/test2", func2)
+
+    groupx := router.Group("v1")
+    groupx.GET("/test1", func3)
     // 绑定端口是8888
     router.Run(":8086")
 }
